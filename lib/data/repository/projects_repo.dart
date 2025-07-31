dddc629c2354c560762a7875b4b214146d712398
Daniel Dickson Dillimono <76996825+DanielDicksonDillimono@users.noreply.github.com>
2025-07-31 10:23:16 +0200

    added icons

---
+++ lib/data/repository/projects_repo.dart
@@ -0,0 +1,32 @@
+import 'package:portfoliosite/domain/project_model.dart';
+
+class ProjectsRepo {
+  //TODO: Fetch projects from Git Hub or a remote server. For now, we will caveman projects.
+  final List<Project> _projects = [
+    Project(
+      title: 'Body Therapy',
+      description:
+          'A mobile app built with flutter. Gives insight on MSK symptoms, possible cause and remedy.',
+      imageUrl: 'assets/images/bodytherapy/icon.png',
+      projectUrl: 'https://github.com/DanielDicksonDillimono/bodytherapy',
+    ),
+    Project(
+      title: 'Replee',
+      description:
+          'A mobile app built with flutter. Create, share and follow workout routines.',
+      imageUrl: 'assets/images/replee/icon.png',
+      projectUrl: 'https://github.com/DanielDicksonDillimono/bodytherapy',
+    ),
+    //Project(title: title, description: description, imageUrl: imageUrl, projectUrl: projectUrl)
+    Project(
+      title: 'Linux IVI',
+      description:
+          'Invehicle Infotainment Application targeted for linux boards. Built with Qt(C++ and QML). Still needs to be completed',
+      imageUrl: 'assets/images/IVI/icon.png',
+      projectUrl:
+          'https://github.com/DanielDicksonDillimono/in_vehicle_infotainment',
+    ),
+  ];
+
+  List<Project> get projects => List.unmodifiable(_projects);
+}
